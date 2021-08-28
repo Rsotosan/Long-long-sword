f640caaa335838d351124dcf4807f13ecb63f1da
juanignaciobrs <juanignaciobrs@gmail.com>
2021-08-28 15:09:18 +0200

    npcs moving, triggers

---
+++ Long long sword/Assets/Scripts/NPCsControllers/WaterTrigger.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterTrigger : MonoBehaviour
+{
+   public WaterGuyController wg;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+    }
+
+ /*   private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        Debug.Log("colision");
+
+       if (wg.direction_right)
+        {
+
+            wg.direction_right = false;
+        }
+
+       if (wg.direction_right == false)
+        {
+
+            wg.direction_right = true;
+        }
+
+    }
+ */
+
+}
