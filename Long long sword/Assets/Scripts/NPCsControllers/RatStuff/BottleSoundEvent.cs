712f7890998a771a702ced371bab4ab9780ae6ca
juanignaciobrs <juanignaciobrs@gmail.com>
2021-08-31 00:39:08 +0200

    Forest

---
+++ Long long sword/Assets/Scripts/NPCsControllers/RatStuff/BottleSoundEvent.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottleSoundEvent : MonoBehaviour
+{
+    public AudioSource audio1Thumping;
+    public AudioSource audio2Glass;
+
+    public AudioSource audio3Water;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        Debug.Log("botella ha caído");
+
+        StartCoroutine(playSounds());
+
+    }
+    IEnumerator playSounds()
+    {
+
+        Debug.Log("cae");
+        yield return new WaitForSeconds(2.5f);
+        audio2Glass.Play();
+      
+
+        yield return new WaitForSeconds(1);
+
+        audio1Thumping.Play();
+
+   
+
+        yield return new WaitForSeconds(2);
+
+        audio3Water.Play();
+
+    }
+
+
+}
